ed533152d33b00239ea14c0d4af6688926d557c7
Zach Bruggeman <mail@bruggie.com>
2016-03-31 20:56:57 -0700

    Correct URL when unescaped
    thanks Mail.app for auto unescaping URLs :^)

--- app/routes/map.js
+++ app/routes/map.js
@@ -7,7 +7,17 @@ export default Ember.Route.extend({
     }
   },
 
+  setupController () {
+    let match = this.controller.get('match')
+    if (match.indexOf('#') === -1) {
+      this.controller.set('match', match + window.location.hash)
+    }
+  },
+
   model (params) {
+    if (params.match.indexOf('#') === -1) {
+      params.match = params.match + window.location.hash
+    }
     return Ember.RSVP.hash({
       identities: this.store.query('identity', params),
       events: this.store.query('event', params)
